f090f167af03c4103293d168614d35439da0522a
HHllmn <118361459+HHllmn@users.noreply.github.com>
2024-10-11 16:31:14 +0100

    Time: 33 ms (60.51%), Space: 11.8 MB (93.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        alphabet = [0] * 26
+        for c in s:
+            alphabet[ord(c) - ord('a')] += 1
+        for c in t:
+            alphabet[ord(c) - ord('a')] -= 1
+        for i in alphabet:
+            if i != 0:
+                return False
+        return True
+    
