b8a841751b4ee9032967598b2fa0cee48095f8f7
HHllmn <118361459+HHllmn@users.noreply.github.com>
2024-10-12 17:51:49 +0100

    Time: 112 ms (84.6%), Space: 12.9 MB (55.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,14 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
-        """      
-        z = 0
-        for i in xrange(len(nums)):
-            if (nums[i] != 0):
-                nums[i], nums[z] = nums[z],nums[i]
-                z += 1
-        
+        """
 
+        non_zero_index = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index] = nums [i]
+                non_zero_index += 1
+
+        for i in range(non_zero_index, len(nums)):
+            nums[i] = 0     
